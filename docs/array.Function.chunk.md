46aade4188cf47dc4d4c6ebf23d38645a2e68449
soetas <3228891558@qq.com>
2024-11-30 17:34:22 +0800

    feat: update files

--- docs/array.Function.chunk.md
+++ docs/array.Function.chunk.md
@@ -4,6 +4,8 @@
 
 > **chunk**\<`T`\>(`array`, `size`): `never`[]
 
+Creates an array of elements split into groups the length of size
+
 ## Type Parameters
 
 • **T**
@@ -12,14 +14,25 @@
 
 • **array**: `T`[]
 
-array
+The array to process
 
 • **size**: `number` = `1`
 
-size
+The length of each chunk
 
 ## Returns
 
 `never`[]
 
-- xx
+- Returns the new array of chunks
+
+## Since
+
+0.1.2
+
+## Example
+
+```js
+ console.log(chunk([1, 2, 3])) // [[1], [2], [3]]
+
+```
--- lib/array/chunk.ts
+++ lib/array/chunk.ts
@@ -1,8 +1,18 @@
 /**
+ * Creates an array of elements split into groups the length of size
+ * 
+ * @param array - The array to process
+ * @param size - The length of each chunk
+ * @returns - Returns the new array of chunks
+ * @since 0.1.2
+ * 
+ * @example
  *
- * @param array - array
- * @param size - size
- * @returns - xx
+ * ```js
+ *  console.log(chunk([1, 2, 3])) // [[1], [2], [3]]
+ *
+ * ```
+ * 
  */
 export default function chunk<T>(array: T[], size = 1) {
   return []
