81d0734e6dc238fc5ddfa1cdf0936d6d381bc544
soetas <3228891558@qq.com>
2025-10-15 20:22:49 +0800

    init project

--- typedoc.config.js
+++ typedoc.config.js
@@ -1,6 +1,18 @@
 /** @type {Partial<import("typedoc").TypeDocOptions>} */
 const config = {
-  'out':'docs'
+  entryPoints:[
+    'src/array/index.ts',
+    'src/assert/index.ts',
+    'src/async/index.ts',
+    'src/date/index.ts',
+    'src/error/index.ts',
+    'src/function/index.ts',
+    'src/math/index.ts',
+    'src/object/index.ts',
+    'src/string/index.ts',
+    'src/utility/index.ts'
+  ],
+  out:'docs'
 }
 
 export default config
